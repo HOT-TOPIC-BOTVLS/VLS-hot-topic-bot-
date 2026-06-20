c554191c86611aa7146f02180d7de8cd07ed40c0
HOT-TOPIC-BOTVLS <femboyhere88@gmail.com>
2026-06-20 11:23:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ intents.message_content = True
 intents.members = True
 intents.guilds = True
 intents.voice_states = True
-intents.reactions= true
+intents.reactions= True
 
 bot = commands.Bot(
     command_prefix=BOT_PREFIX,
